20c7f0480ffa13ff6abe06e9fc05556aed320de5
Elakkiya KM <elakkiyakmfuture@gmail.com>
2024-02-26 00:36:16 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# REACT PRICE CARD TASK 
+**************************
+## ROAD MAP DAY 22 TASK
+***************************
+This react price card task is designed to be responsive and three different price range was built in a dynamic way with (React+Vite), JSX and Bootstrap.
+*******************
+Demo link - (click here)[]
---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import './App.css'; // Import the CSS file for styling
+import Card from './components/Card'; 
+
+const data = [
+  {
+    title: "FREE",
+    price: "$0",
+    user: "Single User",
+    storage: "50GB Storage",
+    domain: "Free Domain",
+    project: "text-muted",
+    projectIcon: "fas fa-times",
+    phone: "text-muted",
+    phoneIcon: "fas fa-times",
+    subDomain: "text-muted",
+    subDomainIcon: "fas fa-times",
+    monthly: "text-muted",
+    monthlyIcon: "fa fa-times",
+  },
+  {
+    title: "PLUS",
+    price: "$9",
+    user: "5 Users",
+    storage: "50GB Storage",
+    domain: "Free Domain",
+    project: "",
+    projectIcon: "fas fa-check",
+    phone: "",
+    phoneIcon: "fas fa-check",
+    subDomain: "",
+    subDomainIcon: "fas fa-check",
+    monthly: "text-muted",
+    monthlyIcon: "fa fa-times",
+  },
+  {
+    title: "PRO",
+    price: "$49",
+    user: "Unlimited Users",
+    storage: "50GB Storage",
+    domain: "Un",
+    project: "",
+    projectIcon: "fas fa-check",
+    phone: "",
+    phoneIcon: "fas fa-check",
+    subDomain: "",
+    subDomainIcon: "fas fa-check",
+    monthly: "",
+    monthlyIcon: "fa fa-check",
+  },
+];
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container bg-primary h-100 my-5 p-4">
+        <div className="row">
+          {data.map((item, index) => {
+            return (
+              <Card key={index} cardData={item} /> // Use Card component here
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
